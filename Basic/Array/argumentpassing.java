d80c3b2d368fbcce0cb6b4d526f67a651d4875e2
Ajaychaudhary9670 <ajajaychaudhary1616@gmail.com>
2026-02-10 13:43:02 +0530

    ..

---
+++ Basic/Array/argumentpassing.java
@@ -0,0 +1,16 @@
+public class argumentpassing {
+    public static void updatearr(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = arr[i]+1;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {23,45,67,};
+        updatearr(arr);
+        for(int i=0;i<arr.length ;i++){
+           System.out.print(arr[i] +" ");                                     // System.out.println(arr[i]);
+        }
+
+    }
+    
+}
