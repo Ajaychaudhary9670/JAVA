f01e09472de6993f230467b88ffe72149b3b4062
Ajaychaudhary9670 <ajajaychaudhary1616@gmail.com>
2026-02-20 23:23:43 +0530

    bit

--- Bit/count1inNum.java
+++ Bit/count1inNum.java
@@ -1,12 +1,11 @@
 public class count1inNum {
     public static int countone(int n) {
         int count = 0;
-        int bitMash = 1 << 0;
-        while (n >0) {
-            if ((n&1)!= 0) {
+        while (n > 0) {
+            if ((n & 1) != 0) {
                 count++;
             }
-             n = n>>1;
+            n = n >> 1;
 
         }
         return count;
